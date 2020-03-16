d381b9f97e1a58a279f6f6bca7af9cc239650074
RashmiNarayan85 <62227747+RashmiNarayan85@users.noreply.github.com>
2020-03-16 14:52:26 +1100

    Update TestUtil.java

--- src/main/java/com/intershop/saig/util/TestUtil.java
+++ src/main/java/com/intershop/saig/util/TestUtil.java
@@ -52,38 +52,4 @@ public class TestUtil {
 		}
 			
 		
-		/*
-		  public static Object[][] getTestData(String sheetName) throws InvalidFormatException {
-			FileInputStream file = null;
-			BufferedInputStream bis = null;
-			try {
-				file = new FileInputStream(TESTDATA_SHEET_PATH);
-				
-				// FileInputStream file = new FileInputStream(TESTDATA_SHEET_PATH);
-				
-
-				bis = new BufferedInputStream(file);
-				
-				} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			}
-			
-			try {
-				book = WorkbookFactory.create(bis);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			sheet = book.getSheet(sheetName);
-			Object[][] data = new Object[sheet.getLastRowNum()][sheet.getRow(0).getLastCellNum()];
-			
-			for(int i=0; i<sheet.getLastRowNum(); i++) {
-				for (int k=0; k<sheet.getRow(0).getLastCellNum(); k++) {
-					data[i][k] = sheet.getRow(i + 1).getCell(k).toString();
-				}
-			}
-			
-			return data;
-		 
-		  }
-		*/
 	}
